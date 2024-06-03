0798a97fefa9cd5b46dc874abff0d72cd7df5e6c
Quinnga <156713253+Quinngaa@users.noreply.github.com>
2024-06-03 23:09:06 +0700

    Add feature recharge energy by drinking milk

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -12,7 +12,7 @@ public class EventHandler {
         int map = 0;
         int col = 0;
         int row = 0;
-        while (map < gp.maxMap && row < gp.maxWorldRow && col < gp.maxWorldCol){
+        while (map < gp.maxMap && row < gp.maxWorldRow && col < gp.maxWorldCol) {
             eventRect[map][row][col] = new EventRect();
             eventRect[map][row][col].x = 23;
             eventRect[map][row][col].y = 23;
@@ -22,7 +22,7 @@ public class EventHandler {
             eventRect[map][row][col].eventRectDefaultY = eventRect[map][row][col].y;
 
             col++;
-            if (col == gp.maxWorldCol){
+            if (col == gp.maxWorldCol) {
                 col = 0;
                 row++;
             }
@@ -33,25 +33,24 @@ public class EventHandler {
         if (gp.keyHandler.AnnouceCompleteAnimation) {
             count++;
             if (count == 1) {
-                if(gp.player.inventory.size() < gp.player.maxInventorySize){
-                    if (gp.player.physical >= level_Rod)
-                    {
+                if (gp.player.inventory.size() < gp.player.maxInventorySize) {
+                    if (gp.player.physical >= level_Rod) {
                         expendPhysical(level_Rod);
-                        gp.ui.range_Y = gp.ui.random.nextInt(gp.ui.bar_Y + 7*gp.tileSize - gp.ui.heightOfRange - 20 - gp.ui.bar_Y - 10 + 1) + gp.ui.bar_Y + 15;
+                        gp.ui.range_Y = gp.ui.random.nextInt(
+                                gp.ui.bar_Y + 7 * gp.tileSize - gp.ui.heightOfRange - 20 - gp.ui.bar_Y - 10 + 1)
+                                + gp.ui.bar_Y + 15;
                         gp.ui.speedOfRange = -gp.ui.speedOfRange;
                         gp.gameState = gp.fishingState;
-                    }
-                    else
-                    {
-                        //Out Of Energy
+                    } else {
+                        // Out Of Energy
                         outOfEnergy(gp.notificationState);
 
-                        //GameOver
-                        if(gp.player.coin < 5 || gp.player.searchItemInInventory( "Milk") == 100){
+                        // GameOver
+                        if (gp.player.coin < 5 || gp.player.searchItemInInventory("Milk") == 100) {
                             gameOver(gp.gameOverState);
                         }
                     }
-                }else{
+                } else {
                     fullOfInventory(gp.notificationState);
                 }
             }
@@ -59,23 +58,23 @@ public class EventHandler {
         if (!gp.keyHandler.AnnouceCompleteAnimation) {
             count = 0;
         }
-//        if (hit(20, 12, "Up")){
-//
-//        }
+        // if (hit(20, 12, "Up")){
+        //
+        // }
     }
 
-    public boolean hit(int map, int col, int row, String reqDirection){
-        boolean hit =false;
+    public boolean hit(int map, int col, int row, String reqDirection) {
+        boolean hit = false;
 
-        if (map == gp.currentMap){
+        if (map == gp.currentMap) {
             gp.player.solidArea.x = (int) (gp.player.worldX + gp.player.solidArea.x);
             gp.player.solidArea.y = (int) (gp.player.worldY + gp.player.solidArea.y);
 
-            eventRect[map][row][col].x = row*gp.tileSize +eventRect[map][row][col].x;
-            eventRect[map][row][col].y = col*gp.tileSize + eventRect[map][row][col].y;
+            eventRect[map][row][col].x = row * gp.tileSize + eventRect[map][row][col].x;
+            eventRect[map][row][col].y = col * gp.tileSize + eventRect[map][row][col].y;
 
-            if (gp.player.solidArea.intersects(eventRect[map][row][col])){
-                if (gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")){
+            if (gp.player.solidArea.intersects(eventRect[map][row][col])) {
+                if (gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
                     hit = true;
                 }
             }
@@ -89,23 +88,31 @@ public class EventHandler {
         return hit;
     }
 
-    public void expendPhysical(int level_Rod)
-    {
+    public void expendPhysical(int level_Rod) {
         gp.player.physical -= level_Rod;
     }
-    public void outOfEnergy(int game_State)
-    {
+
+    public void addPhysical(int amount) {
+        if (gp.player.physical <= gp.player.maxPhysical - amount) {
+            gp.player.physical += amount;
+        } else {
+            gp.player.physical = gp.player.maxPhysical;
+        }
+    }
+
+    public void outOfEnergy(int game_State) {
         gp.gameState = game_State;
         gp.ui.currentTittle = "OOPS!";
         gp.ui.currentNotification = "No more energy";
     }
-    public void fullOfInventory(int game_State)
-    {
+
+    public void fullOfInventory(int game_State) {
         gp.gameState = game_State;
         gp.ui.currentTittle = "OOPS!";
         gp.ui.currentNotification = "You cannot carry any more!";
     }
-    public void gameOver(int game_State){
+
+    public void gameOver(int game_State) {
         gp.gameState = game_State;
     }
 }
