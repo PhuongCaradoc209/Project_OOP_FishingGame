b6e79a285441f8ae9f5fb7eb56a0e11a2deccfa1
= <phuongnguyen20904@gmail.com>
2024-06-05 12:40:50 +0700

    Update Tree images for map

---
+++ src/object/OBJ_Tree_1.java
@@ -0,0 +1,21 @@
+package object;
+
+import main.GamePanel;
+import entity.Entity;
+
+public class OBJ_Tree_1 extends Entity {
+    public OBJ_Tree_1(GamePanel gp) {
+        super(gp);
+        size = gp.tileSize * 3;
+        name = "Tree";
+        down1 = setup("object/tree", 96, 96);
+        collision = true;
+
+        solidArea.x = gp.tileSize/2;
+        solidArea.y = gp.tileSize;
+        solidArea.width = gp.tileSize + gp.tileSize/2 + 10;
+        solidArea.height = gp.tileSize + gp.tileSize/2;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
+    }
+}
